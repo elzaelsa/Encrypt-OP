4789d538fb43e3927c73577af883d1e17ab802e5
elzaelza <muhamedovaelza@gmail.com>
2022-05-28 22:12:47 +0300

    full program

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def perform_by_type(text, elem_len, cur_len, elem_type):
+def perform_by_type(text, elem_len, cur_len):
     if elem_type == '1' or elem_len == '1':
         # длина хвоста без группировки
         ost_len = len(text) % cur_len
@@ -21,17 +21,15 @@ def perform_by_type(text, elem_len, cur_len, elem_type):
     return text
 
 
-def convert_by_mode(text, connect_symb, cur_len, mode, key):
+def convert_by_mode(text, connect_symb, cur_len):
     if mode == '1':
         # собираем по группам ключа строки в порядке ключа, затем по группам всей строки
         result = connect_symb.join(
-            [connect_symb.join([text_part[key.index(str(i))] for i in range(cur_len)]) for text_part in text]
-        )
+            [connect_symb.join([text_part[key.index(str(i))] for i in range(cur_len)]) for text_part in text])
     elif mode == '2':
         # формируем строки по элементам ключа и по группа всей строки
-        result = connect_symb.join(
-            [connect_symb.join([text_part[int(i)] for i in key]) for text_part in text]
-        ).replace('\0', '')
+        result = connect_symb.join([connect_symb.join([text_part[int(i)] for i in key]) for text_part in text]).replace(
+            '\0', '')
 
     return result
 
@@ -43,13 +41,46 @@ def encryption(mode, elem_type, elem_len, key, text):
     cur_len = len(key)
 
     # преобразовываем текст в зависимости от типа шифровки
-    text = perform_by_type(text, elem_len, cur_len, elem_type)
+    text = perform_by_type(text, elem_len, cur_len)
 
     # разбиваем строку максимальной по длине ключа
     text = [text[i:i + cur_len] for i in range(0, len(text), cur_len)]
     # выбор режима
 
-    return convert_by_mode(text, connect_symb, cur_len, mode, key)
+    return convert_by_mode(text, connect_symb, cur_len)
 
 
-print(encryption(1, 1, 0, ['1', '3', '0', '2'], 'abcdefet'))
+def is_valid_key(key):
+    for k in key:
+        if not k.isdigit() or len(k) > 1:
+            return False
+    return True
+
+
+# Реализация Меню
+print('Меню:', '1. Шифрование', '2. Разшифровывание', end='\n')
+mode = input('Выберите режим: ')
+# Обработка некорректного ввода
+while mode not in ('1', '2'):
+    mode = input('Пожалуйста, выберите один из вариантов: ')
+print('Виды элемента:', '1. Один символ', '2. Группа символов', '3. Слово', end='\n')
+elem_type = input('Выберите вид элемента: ')
+# Обработка некорректного ввода
+while elem_type not in ('1', '2', '3'):
+    elem_type = input('Пожалуйста, выберите один из вариантов: ')
+if elem_type == '2':
+    elem_len = input('Введите длину блока: ')
+    # Обработка некорректного ввода
+    while not elem_len.isdigit():
+        elem_len = input('Пожалуйста, введите корректное значение: ')
+else:
+    elem_len = ''
+key = input('Введите ключ (через пробел, например - 3 0 2 1):').split(' ')
+# Обработка некорректного ввода
+while not is_valid_key(key):
+    key = input('Пожалуйста, введите корректное значение (например - 3 0 2 1): ').split(' ')
+text = input('Введите сообщение: ')
+
+# шифрование/дешифрование и вывод
+result = encryption(mode, elem_type, elem_len, key, text)
+print(f'\nРезультат: {result}')
